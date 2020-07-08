926fa0a2bacb0097f52f19c8f5e91d3a32b9e296
Melroy <melroy27@hotmail.com>
2020-07-08 15:06:22 +0530

    changes

--- assignment.cpp
+++ assignment.cpp
@@ -1,30 +1,78 @@
-#include<bits/stdc++.h>
-using namespace std; 
-int main(){
-	ios::sync_with_stdio(0);
-	cin.tie(0);
-	cout.tie(0);
-	map<char,int> p;
-	string name,pattern;
-  int t;
-  cin>>t;
-  while(t--){
-      cin>>name>>pattern;
-      for(int i=0;i<pattern.length();i++){
-        p[pattern[i]]++;
-      }
-      int flag = 1;
-      for(int i=0;i<name.length();i++){
-        if(p[name[i]]==0)
-        {
-          cout<<"NO \n";
-          flag=0;
-          break;
-        }
-        else
-          p[name[i]]--;
-      }
-    if(flag==1)
-      cout<<"YES\n";
+#include <iostream>
+#include <string>
+#include <cstring>
+#include <bits/stdc++.h>
+#include<vector>
+using namespace std;
+
+int main()
+{
+  std::vector< string > arr;
+  int x, i;
+  string y="YES";
+  string n="NO";
+  std::cout << "Enter the times you wanna check: ";
+  std::cin >> x;
+
+  for (i = 1; i <= x; i++)
+  {
+    string a, b, c, d, e, f;
+    string ans;
+
+
+    cin >> a;
+    cin >> b;
+
+    string str1 = b;
+    string str2 = a;
+
+    int pos = 0;
+    int index;
+
+    if ((index = str1.find(str2, pos)) != string::npos)
+    {
+      arr.push_back(y);
+      pos = index + 1;
+    }
+    else
+    {
+     arr.push_back(n);
+    }
+
+    cin >> c;
+    cin >> d;
+    if (c == d)
+    {
+      arr.push_back(y);
+    }
+    else
+    {
+      arr.push_back(n);
+    }
+
+
+    cin >> e;
+    cin >> f;
+
+    string reverse = string(f.rbegin(), f.rend());
+
+    string str3 = reverse;
+    string str4 = f;
+
+    int pos1 = 0;
+    int index1;
+
+    if ((index1 = str4.find(str3, pos1)) != string::npos)
+    {
+      cout<<index1;
+      arr.push_back(y);
+      pos1 = index1 + 1;
+    }
+    else
+    {
+     arr.push_back(n);
+    }
+    for(int i=0; i < arr.size(); i++)
+      cout << arr.at(i) << ' ';
   }
 }
